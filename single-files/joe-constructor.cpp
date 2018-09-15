844345e50e41359b47c48f6a30a31c3aac640991
Colton Hurst <colton@coltonhurst.com>
2018-09-15 15:54:11 -0400

    An example of constructors with a Joe object.

---
+++ single-files/joe-constructor.cpp
@@ -0,0 +1,43 @@
+/*
+	The code below is a simple example of constructors.
+	We have the default constructor, which sets a Joe objects age to -1.
+	We have an overloaded constructor, which takes an int 'newAge' and
+	sets the Joe objects age to that value. The Joe object also has a
+	getJoesAge() method, which returns the Joe object's age.
+*/
+
+#include <iostream>
+
+// Class definition
+class Joe
+{
+public:
+	Joe();
+	Joe(int newAge);
+	int getJoesAge();
+private:
+	int age;
+};
+
+// Class implementation
+Joe::Joe() {
+	age = -1;
+}
+
+Joe::Joe(int newAge) {
+	age = newAge;
+}
+
+int Joe::getJoesAge() {
+	return age;
+}
+
+// The main
+int main()
+{
+	Joe joey;
+	Joe joey2(2);
+	std::cout << joey.getJoesAge() << std::endl;
+	std::cout << joey2.getJoesAge() << std::endl;
+	return 0;
+}
