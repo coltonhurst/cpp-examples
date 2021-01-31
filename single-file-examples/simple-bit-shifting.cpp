de21f231bad6c15ea8b5f7addc446c28e02b1fc0
Colton Hurst <colton@coltonhurst.com>
2021-01-30 23:16:38 -0500

    Make bit shifting example more clear

--- single-file-examples/simple-bit-shifting.cpp
+++ single-file-examples/simple-bit-shifting.cpp
@@ -1,5 +1,16 @@
 /*
 	A simple example of bit shifting.
+
+    We start with 4, which in binary is: 0000 0100
+
+    By shifting the bits left one place,
+    we get: 0000 1000 (this is 8)
+
+    Then, we shift the bits right TWO places!
+    We had: 0000 1000
+
+    After the two shifts right we have: 0000 0010
+    This is 2!
 */
 
 #include <iostream>
@@ -7,10 +18,10 @@
 int main() {
 
 	int number = 4;
-	number = number << 1;	// arithmetic shift left, multiplies by 2
+	number = number << 1;	// 1 arithmetic shift left, multiplies by 2
 	std::cout << number << std::endl;
 
-	number = number >> 2;	// arithmetic shift right, divides by 4
+	number = number >> 2;	// 2 arithmetic shifts right, divides by 4
 	std::cout << number << std::endl;
 
 	return 0;
